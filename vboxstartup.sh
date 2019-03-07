775e5e7e59e959afc41925f3cfbec07b57f43ee8
Stellar <init3@tuta.io>
2019-03-07 21:10:00 +0000

    vboxstartup.sh

--- vboxstartup.sh
+++ vboxstartup.sh
@@ -2,6 +2,9 @@
 # -- Start VirtuaBox VM's from CommandLine in Headless Mode -- 
 # 
 
+# As $user 
+#   - If VM's installed as Root (Insecure - avoid), use as sudo user
+
 # Move 'vboxstartup.sh' script to /usr/bin
 
 ----------------------------
@@ -10,15 +13,26 @@
 
 # Script name: vboxstartup.sh
 
-cd /home/user0/scripts/vboxscripts ;
+cd /home/$user/scripts/vboxscripts ;
 ./vm0.sh ;
 ./vm1.sh ;
 ./vm2.sh
 
 ----------------------------
+
 # Script name: vm0.sh
 # etc...
 
 vboxmanage startvm vm0 --type headless
 
 ----------------------------
+
+
+----------------------------
+----------------------------
+
+- Code Improvements:
+
+  - $userName variable input
+      - Poll $userName variable
+  - Input VM name & directory
